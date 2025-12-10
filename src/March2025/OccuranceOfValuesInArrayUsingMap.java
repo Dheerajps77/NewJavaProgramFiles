ec92b232f86d3eda99ae3168e2b4821ed60c9889
Dheerajps77 <39496037+Dheerajps77@users.noreply.github.com>
2025-12-10 12:43:22 +0530

    Update OccuranceOfValuesInArrayUsingMap.java

--- src/March2025/OccuranceOfValuesInArrayUsingMap.java
+++ src/March2025/OccuranceOfValuesInArrayUsingMap.java
@@ -10,6 +10,31 @@ public class OccuranceOfValuesInArrayUsingMap {
 		// findMinOccuranceOfEachCharInString();
 	}
 
+
+public static void findMaxOccuranceOfEachCharInString() {
+
+    String str = "Dheeraj Pratap Singhd";
+    Map<Character, Integer> map = new HashMap<>();
+
+    for (char c : str.toCharArray()) {
+        map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    int maxCount = 0;
+    char maxChar = ' ';
+
+    for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+        if (entry.getValue() > maxCount) {
+            maxCount = entry.getValue();
+            maxChar = entry.getKey();
+        }
+    }
+
+    System.out.println("Actual Value : " + str);
+    System.out.println("Max occurrence of char : " + maxChar +
+                       " and max occurrence count : " + maxCount);
+}
+
 	public static void findMaxOccuranceOfEachCharInString() {
 		try {
 
